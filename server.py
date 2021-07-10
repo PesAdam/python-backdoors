05f8477761d233aeef232c41ce4e6bea09c77c85
pes adam <pesadam15@gmail.com>
2021-07-10 17:17:42 +0200

    server.py

--- server.py
+++ server.py
@@ -1,3 +1,11 @@
 import socket
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(('#your ip', 5555))
+print('[+] waiting')
+sock.listen(5)
+
+target, ip = sock.accept()
+print('[+] ready' + str(ip))
+target_communication()
+
